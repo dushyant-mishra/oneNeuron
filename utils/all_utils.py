c84e8549c4ed657e77b831fb130db6c4ed1ef24c
dushyant-mishra <dushyant.mishra@gmail.com>
2022-03-15 14:47:15 -0400

    updated repo

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import joblib # FOR SAVING MY MODEL AS A BINARY FILE
+import joblib # for saving my model as binary file
 from matplotlib.colors import ListedColormap
 import os
 import logging
-plt.style.use("fivethirtyeight") # THIS IS STYLE OF GRAPHS
+plt.style.use("fivethirtyeight") # this is the style of the plot
 
-def prepare_data(df):
+def prepare_data(df, target_col="y"):
 
   """it is used to separate the dependent variables and independent features
 
@@ -19,9 +19,9 @@ def prepare_data(df):
   """
   logging.info("Preparing the data by segregating the dependent and independent variables")
 
-  X = df.drop("y", axis=1)
+  X = df.drop(target_col, axis=1) #axis=1 means that it will drop the y column in this case
 
-  y = df["y"]
+  y = df[target_col]
 
   return X, y
 
@@ -35,10 +35,12 @@ def save_model(model, filename):
   """
   logging.info("saving the trained model")
   model_dir = "models"
-  os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  filePath = os.path.join(model_dir, filename) # model/filename
+  os.makedirs(model_dir, exist_ok=True) # Only create a new directory if model_dir does not exist
+  filePath = os.path.join(model_dir, filename) # models/filename
   joblib.dump(model, filePath)
   logging.info(f"saved the trained model to {filePath}")
+
+
 def save_plot(df, file_name, model):
   """saves the plots
 
@@ -47,22 +49,22 @@ def save_plot(df, file_name, model):
       file_name: its a path to save the plots
       model: trained model
   """
-  def _create_base_plot(df):
+  def _create_base_plot(df):       #plots the data points and creates a base plot
     logging.info("creating the base plot")
     df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
     plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
     plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
-    figure = plt.gcf() # get current figure
+    figure = plt.gcf()    # get current figure
     figure.set_size_inches(10, 8)
 
-  def _plot_decision_regions(X, y, classfier, resolution=0.02):
+  def _plot_decision_regions(X, y, classfier, resolution=0.02):  #plots the decision boundaries
     logging.info("plotting the decision regions")
-    colors = ("red", "blue", "lightgreen", "gray", "cyan")
-    cmap = ListedColormap(colors[: len(np.unique(y))])
+    colors = ("magenta", "green", "lightgreen", "gray", "cyan")
+    cmap = ListedColormap(colors[: len(np.unique(y))]) # since we have a binary classifier (perceptron) but listed 4 colors above it will take the first two colors for y output values
 
-    X = X.values # as a array
-    x1 = X[:, 0] 
-    x2 = X[:, 1]
+    X = X.values # this is the x1 and x2 values as an array
+    x1 = X[:, 0] # first column 
+    x2 = X[:, 1] # second column
     x1_min, x1_max = x1.min() -1 , x1.max() + 1
     x2_min, x2_max = x2.min() -1 , x2.max() + 1  
 
@@ -73,7 +75,7 @@ def save_plot(df, file_name, model):
     #print(xx1.ravel().T, end='\n')
     #print(xx1.shape, end='\n')
     
-    Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)  #ravel converts to 1D array
     Z = Z.reshape(xx1.shape)
     #print(Z.reshape(xx1.shape), end='\n')
 
@@ -89,7 +91,7 @@ def save_plot(df, file_name, model):
   _plot_decision_regions(X, y, model)
 
   plot_dir = "plots"
-  os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  plotPath = os.path.join(plot_dir, file_name) # model/filename
+  os.makedirs(plot_dir, exist_ok=True) # Only create if plot_dir doesen't exist
+  plotPath = os.path.join(plot_dir, file_name) # models/filename
   plt.savefig(plotPath)
-  logging.info(f"saving the plot at {plotPath}")  
+  logging.info(f"saving the plot at {plotPath}")
--- utils/model.py
+++ utils/model.py
@@ -3,22 +3,23 @@ import logging
 from tqdm import tqdm
 
 class Perceptron:
-  def __init__(self, eta, epochs):
-    self.weights = np.random.randn(3) * 1e-4 # SMALL WEIGHT INIT
+  def __init__(self, eta: float=None, epochs: int=None):
+    self.weights = np.random.randn(3) * 1e-4 # Small Weight Initialization
     logging.info(f"initial weights before training: \n{self.weights}")
-    self.eta = eta # LEARNING RATE
-    self.epochs = epochs 
+    self.eta = eta # Learning Rate
+    self.epochs = epochs #Iterations
+  
+  def _z_outcome(self, inputs, weights):
+    return np.dot(inputs, weights) # z = w * x
 
-
-  def activationFunction(self, inputs, weights):
-    z = np.dot(inputs, weights) # z = W * X
-    return np.where(z > 0, 1, 0) # CONDITION, IF TRUE, ELSE
+  def activationFunction(self, z):
+    return np.where(z > 0, 1, 0) # Condition, if true, else
 
   def fit(self, X, y):
     self.X = X
     self.y = y
 
-    X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # CONCATINATION
+    X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # Concatenate
     logging.info(f"X with bias: \n{X_with_bias}")
 
     for epoch in tqdm(range(self.epochs), total=self.epochs, desc="training the model"):
@@ -26,18 +27,24 @@ class Perceptron:
       logging.info(f"for epoch: {epoch}")
       logging.info("--"*10)
 
-      y_hat = self.activationFunction(X_with_bias, self.weights) # foward propagation
+      z = self._z_outcome(X_with_bias, self.weights)
+      y_hat = self.activationFunction(z) # foward propagation
       logging.info(f"predicted value after forward pass: \n{y_hat}")
+
       self.error = self.y - y_hat
       logging.info(f"error: \n{self.error}")
+
+      total_loss = np.sum(self.error)
+      logging.info(f"total loss: {total_loss}")      # returns the sum of all the errors 
+
       self.weights = self.weights + self.eta * np.dot(X_with_bias.T, self.error) # backward propagation
       logging.info(f"updated weights after epoch:\n{epoch}/{self.epochs} : \n{self.weights}")
       logging.info("#####"*10)
 
-
   def predict(self, X):
-    X_with_bias = np.c_[X, -np.ones((len(X), 1))]
-    return self.activationFunction(X_with_bias, self.weights)
+    X_with_bias = np.c_[X, -np.ones((len(X), 1))]  #prediction is same as forward pass without the backward pass
+    z= self._z_outcome(X_with_bias, self.weights)
+    return self.activationFunction(z)
 
   def total_loss(self):
     total_loss = np.sum(self.error)
